a47d48ff6456aa7733eb4570ffe9cc8b999d355e
Bugsyaya <marina.blin@free.fr>
2018-05-07 16:50:34 +0200

    Correction des routes pour Lieu

--- app/controllers/LieuController.scala
+++ app/controllers/LieuController.scala
@@ -5,7 +5,7 @@ import javax.inject.Inject
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import database.{DBDriverENI, ENIConf, ENIDB}
-import models.{Cours, Lieu, UniteFormation}
+import models.Lieu
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import scala.concurrent.ExecutionContext
