79b1fc45e86c18890ef4fcb14b2d5523867af241
pbontempi2017 <philippe.bontempi2017@campus-eni.fr>
2018-05-08 14:08:11 +0200

    Merge remote-tracking branch 'origin/master' into lectureBaseExternalisationCoSql
    # Conflicts:
    #	app/controllers/CoursController.scala
    #	app/controllers/FormationController.scala
    #	app/database/DBDriverENI.scala
    #	app/database/ENIDB.scala
    #	app/models/Cours.scala
    #	app/models/Formation.scala
    #	app/models/Module.scala
    #	app/models/ModuleParUnite.scala
    #	conf/routes

--- app/models/Cours.scala
+++ app/models/Cours.scala
@@ -3,45 +3,41 @@ package models
 import java.sql.ResultSet
 import helper._
 import play.api.libs.json.Json
-import java.util.Date
-import java.text.SimpleDateFormat
-import java.time.{Instant, ZoneId}
 
 object Cours {
 	implicit val format = Json.format[Cours]
 	implicit def fromResultSet(rs: ResultSet): Seq[Cours] = {
 		Utils.results(rs){
 			case r => Cours(
-				r.getDate("Debut").toLocalDate().atStartOfDay(ZoneId.of("UTC")).toInstant(),
-				r.getDate("Fin").toLocalDate().atStartOfDay(ZoneId.of("UTC")).toInstant(),
-				r.getInt("DureeReelleEnHeures"),
-				r.getString("CodePromotion"),
-				r.getString("IdCours"),
-				r.getFloat("PrixPublicAffecte"),
-				r.getInt("IdModule"),
-				r.getString("LibelleCours"),
-				r.getInt("DureePrevueEnHeures"),
-				r.getBoolean("DateAdefinir"),
-				r.getString("CodeSalle"),
-				r.getInt("CodeFormateur"),
-				r.getInt("CodeLieu"),
+				r.getString("debut"),
+				r.getString("fin"),
+				r.getInt("dureeReelleEnHeures"),
+				r.getString("codePromotion"),
+				r.getString("idCours"),
+				r.getFloat("prixPublicAffecte"),
+				r.getInt("idModule"),
+				r.getString("libelleCours"),
+				r.getInt("dureePrevueEnHeures"),
+				r.getBoolean("dateAdefinir"),
+				r.getString("codeSalle"),
+				r.getInt("codeFormateur"),
+				r.getInt("codeLieu")
 			)}.toSeq
-
 	}
 }
 
 case class Cours (
-														debut: Instant,
-														fin: Instant,
-														dureeReelleEnHeures: Int,
-														codePromotion: String,
-														idCours: String,
-														prixPublicAffecte: Float,
-														idModule: Int,
-														libelleCours: String,
-														dureePrevueEnHeures: Int,
-														dateAdefinir: Boolean,
-														codeSalle: String,
-														codeFormateur: Int,
-														codeLieu: Int,
-                          )
+	                 debut: String,
+	                 fin: String,
+	                 dureeReelleEnHeures: Int,
+	                 codePromotion: String,
+	                 idCours: String,
+	                 prixPublicAffecte: Float,
+	                 idModule: Int,
+	                 libelleCours: String,
+	                 dureePrevueEnHeures: Int,
+	                 dateAdefinir: Boolean,
+	                 codeSalle: String,
+	                 codeFormateur: Int,
+	                 codeLieu: Int
+                 )
