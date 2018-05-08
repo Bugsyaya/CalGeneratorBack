5d537932a6ad46a491e3e28544c678f0b7e3bc47
pbontempi2017 <philippe.bontempi2017@campus-eni.fr>
2018-05-08 14:54:42 +0200

    Merge remote-tracking branch 'origin/master' into lectureBaseExternalisationCoSql
    # Conflicts:
    #	app/controllers/CoursController.scala
    #	app/controllers/FormationController.scala
    #	app/database/DBDriverENI.scala
    #	app/database/ENIDB.scala
    #	app/models/Cours.scala
    #	app/models/Formation.scala
    #	app/models/Module.scala
    #	app/models/ModuleParUnite.scala
    #	conf/routes

--- app/models/Probleme.scala
+++ app/models/Probleme.scala
@@ -3,22 +3,11 @@ package models
 import java.time.Instant
 
 case class Probleme (
-                        // ces instant ne seront pas utiliser au travers de l'API, on peut les garder ? stp
-                        debutFormation: Instant = Instant.now(),
-                        finFormation: Instant = Instant.now(),
+                        periodeFormation : Periode,
                         formation: Formation,
                         cours: Seq[Cours],
+                        contraintes : Seq[Contrainte]
+
                         // Partie contrainte
-                        lieux: Seq[Lieu],
-                        nbHeureAnnuel: Int = 0,
-                        dureeMaxFormation : Int = 0,
-                        maxSemaineFormation: Int = 0,
-                        maxStagiaireEnFormationParCours: Int = 0
-                        // todo
-                        // Il reste périodes de formation à exclure
-                        // Peut être une autre liste d'une case class avec date de début et date de fin
-                        // Période de formation obligatoire, peut-être la même case class
-                        // Stagiaires dont la présence est requise
-                        // une autre case class stagaire avec le calendrier qui la liste des cours à suivre?
-                        //
+
                       )
